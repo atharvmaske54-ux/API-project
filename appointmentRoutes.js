4302b9b173e443f5d075a3c98c75129663aa3d1f
Atharv Maske <atharvmaske54@gmail.com>
2026-06-19 16:31:55 +0530

    Delete appointmentRoutes.js

--- appointmentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getAvailableSlots } = require("./appointmentController");
-
-const router = express.Router();
-
-// Route to get available slots
-router.get("/available-slots", getAvailableSlots);
-
-module.exports = router;
