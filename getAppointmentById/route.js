346485b682d4661bf1873e5492cdcd574f8fe007
Delvyn Nadar <1.gojou.saturo7@gmail.com>
2026-06-21 16:53:06 +0530

    Delete getAppointmentById directory

--- getAppointmentById/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAppointmentById } = require('./controller');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/:id', protect, getAppointmentById);
-
-module.exports = router;
