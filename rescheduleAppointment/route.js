7265f8c1d509dcd8023fee0ad7dce564c2906e8c
Delvyn Nadar <1.gojou.saturo7@gmail.com>
2026-06-21 16:53:44 +0530

    Delete rescheduleAppointment directory

--- rescheduleAppointment/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { rescheduleAppointment } = require('./controller');
-const { protect } = require('../middleware/authMiddleware');
-
-router.put('/:id', protect, rescheduleAppointment);
-
-module.exports = router;
