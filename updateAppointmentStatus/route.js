3fd34b19f1508e87cb48717b0036da8706d028db
Delvyn Nadar <1.gojou.saturo7@gmail.com>
2026-06-21 16:52:18 +0530

    Delete updateAppointmentStatus directory

--- updateAppointmentStatus/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { updateAppointmentStatus } = require('./controller');
-const { protect } = require('../middleware/authMiddleware');
-
-router.put('/:id', protect, updateAppointmentStatus);
-
-module.exports = router;
