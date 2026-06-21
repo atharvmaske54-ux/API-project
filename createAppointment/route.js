c49d1e3cbac45b05d65aec4ddac5ce312a4fbb4b
Delvyn Nadar <1.gojou.saturo7@gmail.com>
2026-06-21 16:52:39 +0530

    Delete createAppointment directory

--- createAppointment/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createAppointment } = require('./controller');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/', protect, createAppointment);
-
-module.exports = router;
